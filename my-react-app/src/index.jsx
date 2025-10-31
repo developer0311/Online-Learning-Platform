5ba44fe7ea9cf7c4668aab7b706eadf0bcaa7bc4
Diprati Das <dipratidas2004@gmail.com>
2025-10-31 14:27:23 +0530

    update folder structure

--- my-react-app/src/index.jsx
+++ my-react-app/src/index.jsx
@@ -1,5 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
 
 import "../public/css/index.css";
 import "../public/css/media.css";
